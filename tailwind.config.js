429790308db8e28eed66d2b32d236438da4d949d
Tolgaycode <tolgaycode@gmail.com>
2023-10-26 21:04:35 +0300

    added privet page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
       fontFamily: {
         Raleway: ["Raleway", "sans-serif"],
       },
+      colors: {
+        headerColor: "#515BB8",
+        homeColor: "#5661C6",
+        textBlack: "#1B1B1E",
+      },
+      letterSpacing: {
+        widestx2: ".25em",
+      },
+      spacing: {
+        pageFull: "56rem",
+      },
     },
   },
   plugins: [],
