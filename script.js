ba52cc6ef5b59c7ada8a84a1f7410fd5be4ec323
Tolgaycode <tolgaycode@gmail.com>
2023-11-28 14:31:09 +0300

    update

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ document.addEventListener("DOMContentLoaded", function () {
     background.style.top = -scrollValue * 0.3 + "px";
   });
 });
+
+const hamburgerMenu = document.querySelector(".hamburgerMenu");
+const closeButton = document.querySelector(".closeButton");
+const sidebar = document.querySelector(".sidebar");
+
+hamburgerMenu.addEventListener("click", function () {
+  sidebar.classList.remove("invisible");
+  sidebar.classList.add("visible");
+});
+
+closeButton.addEventListener("click", function () {
+  sidebar.classList.remove("visible");
+  sidebar.classList.add("invisible");
+});
